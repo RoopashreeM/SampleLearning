a4e3a9e9b8ecb538042549efbada446629c2f8d2
roopashree munegowda <roopashreemunegowda@roopashrees-MacBook-Pro-2.local>
2019-06-25 11:59:33 -0700

    error cleared

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 v=input('Do you wish to recive greetings?')
 if v=='yes':
       print("hello")
+<<<<<<< HEAD
       print("hi")
       print('how is it going')
       print('have a great day')
 
+=======
+>>>>>>> parent of 0812a17... hi added
 else:
       print("thank you")
 
